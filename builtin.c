abf35890d9ac914980b8f7916294669d9b6f375d
Tewachew Saleigizer <123402812+tewa92@users.noreply.github.com>
2023-12-13 13:46:09 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -51,37 +51,38 @@ int _myexit(info_t *inform)
  */
 int _mycd(info_t *inform)
 {
-	char *st, *dir, buffer[1024];
+	char *s, *dir, buffer[1024];
 	int chdir_ret;
 
 	/* Get the current working directory */
-	st = getcwd(buffer, 1024);
+	s = getcwd(buffer, 1024);
 
 	/**
 	 * Print an error message if getcwd fails (TODO:
 	 * Add a meaningful error message)
 	 */
-	if (!st)
-	_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
 
 	/* If no directory argument is provided */
 	if (!inform->argv[1])
 	{
 	/* Get the HOME directory and change to it, or use "/" if HOME is not set */
-	dir = _getenv(inform, "HOME=");
-	if (!dir)
-	chdir_ret = /* TODO: what should this be? */
-	chdir((dir = _getenv(inform, "PWD=")) ? dir : "/");
-	else
-		chdir_ret = chdir(dir);
+		dir = _getenv(inform, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(inform, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
 	}
 	/* If the argument is "-", change to the previous working directory */
 	else if (_strcmp(inform->argv[1], "-") == 0)
 	{
 	/* If OLDPWD is not set, print an error message */
-	if (!_getenv(inform, "OLDPWD="))
-	{
-		_puts("TODO: >>getcwd failure emsg here<<\n");
+		if (!_getenv(inform, "OLDPWD="))
+		{
+			_puts(s);
+			_putschar('\n');
 		return (1);
 	}
 
@@ -112,7 +113,7 @@ int _mycd(info_t *inform)
 
 	/* Return 0 on successful directory change, 1 on error */
 
-	return (chdir_ret == -1 ? 1 : 0);
+	return (0);
 } /* End of _mycd function */
 
 
