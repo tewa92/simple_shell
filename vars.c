2cf7187fcf6ce190111205671dcd34e5bd359b54
Tewachew Saleigizer <123402812+tewa92@users.noreply.github.com>
2023-12-13 16:54:33 +0300

    Update vars.c

--- vars.c
+++ vars.c
@@ -17,27 +17,27 @@ int is_chain(info_t *information, char *buffer, size_t *position)
 {
 size_t index = *position;
 
-if (buffer[index] == '|' && buffer[index + 1] == '|')
-{
-buffer[index] = 0;
-index++;
-information->cmd_buf_type = CMD_OR;
-}
-else if (buffer[index] == '&' && buffer[index + 1] == '&')
-{
-buffer[index] = 0;
-index++;
-information->cmd_buf_type = CMD_AND;
-}
-else if (buffer[index] == ';') /* Found end of this command */
-{
-buffer[index] = 0; /* Replace semicolon with null */
-information->cmd_buf_type = CMD_CHAIN;
-}
-else
-return (0);
+  if (buffer[index] == '|' && buffer[index + 1] == '|')
+  {
+    buffer[index] = 0;
+    index++;
+    information->cmd_buf_type = CMD_OR;
+  }
+  else if (buffer[index] == '&' && buffer[index + 1] == '&')
+  {
+    buffer[index] = 0;
+    index++;
+    information->cmd_buf_type = CMD_AND;
+  }
+  else if (buffer[index] == ';') /* Found end of this command */
+  {
+    buffer[index] = 0; /* Replace semicolon with null */
+    information->cmd_buf_type = CMD_CHAIN;
+  }
+  else
+    return (0);
 
-*position = index;
+  *position = index;
 return (1);
 }
 
@@ -56,26 +56,26 @@ return (1);
 */
 void check_chain(info_t *information, char *buffer, size_t *position, size_t index, size_t loop_length)
 {
-size_t loop_index = *position;
+  size_t loop_index = *position;
 
-if (information->cmd_buf_type == CMD_AND)
-{
-if (information->status)
-{
-buffer[index] = 0;
-loop_index = loop_length;
-}
-}
-if (information->cmd_buf_type == CMD_OR)
-{
-if (!information->status)
-{
-buffer[index] = 0;
-loop_index = loop_length;
-}
-}
+  if (information->cmd_buf_type == CMD_AND)
+  {
+    if (information->status)
+    {
+      buffer[index] = 0;
+      loop_index = loop_length;
+    }
+  }
+  if (information->cmd_buf_type == CMD_OR)
+  {
+    if (!information->status)
+    {
+      buffer[index] = 0;
+      loop_index = loop_length;
+    }
+  }
 
-*position = loop_index;
+  *position = loop_index;
 }
 
 
@@ -90,31 +90,25 @@ loop_index = loop_length;
 */
 int replace_alias(info_t *information)
 {
-int iteration;
-list_t *aliasNode;
-char *aliasValue;
+  int iteration;
+  list_t *aliasNode;
+  char *aliasValue;
 
-for (iteration = 0; iteration < 10; iteration++)
-{
-aliasNode = node_starts_with(information->alias, information->argv[0], '=');
-if (!aliasNode)
-return (0);
-
-free(information->argv[0]);
-aliasValue = _strchr(aliasNode->str, '=');
-
-if (!aliasValue)
-return (0);
-
-aliasValue = _strdup(aliasValue + 1);
-
-if (!aliasValue)
-return (0);
-
-information->argv[0] = aliasValue;
-}
-
-return (1);
+  for (iteration = 0; iteration < 10; iteration++)
+  {
+    aliasNode = node_starts_with(information->alias, information->argv[0], '=');
+    if (!aliasNode)
+      return (0);
+    free(information->argv[0]);
+    aliasValue = _strchr(aliasNode->str, '=');
+    if (!aliasValue)
+      return (0);
+    aliasValue = _strdup(aliasValue + 1);
+    if (!aliasValue)
+      return (0);
+    information->argv[0] = aliasValue;
+  }
+  return (1);
 }
 
 /**
@@ -128,43 +122,41 @@ return (1);
 */
 int replace_vars(info_t *information)
 {
-int argIndex = 0;
-list_t *envNode;
+  int argIndex = 0;
+  list_t *envNode;
 
-for (argIndex = 0; information->argv[argIndex]; argIndex++)
-{
-char *currentArg = information->argv[argIndex];
+  for (argIndex = 0; information->argv[argIndex]; argIndex++)
+  {
+    char *currentArg = information->argv[argIndex];
+    if (currentArg[0] != '$' || !currentArg[1])
+      continue;
 
-if (currentArg[0] != '$' || !currentArg[1])
-continue;
+    if (!_strcmp(currentArg, "$?"))
+    {
+      replace_string(&(information->argv[argIndex]),
+        _strdup(convert_number(information->status, 10, 0)));
+      continue;
+    }
 
-if (!_strcmp(currentArg, "$?"))
-{
-replace_string(&(information->argv[argIndex]),
-_strdup(convert_number(information->status, 10, 0)));
-continue;
-}
+    if (!_strcmp(currentArg, "$$"))
+    {
+      replace_string(&(information->argv[argIndex]),
+        _strdup(convert_number(getpid(), 10, 0)));
+      continue;
+    }
 
-if (!_strcmp(currentArg, "$$"))
-{
-replace_string(&(information->argv[argIndex]),
-_strdup(convert_number(getpid(), 10, 0)));
-continue;
-}
+    envNode = node_starts_with(information->env, &(currentArg[1]), '=');
+    if (envNode)
+    {
+      replace_string(&(information->argv[argIndex]),
+        _strdup(_strchr(envNode->str, '=') + 1));
+      continue;
+    }
 
-envNode = node_starts_with(information->env, &(currentArg[1]), '=');
+    replace_string(&(information->argv[argIndex]), _strdup(""));
+  }
 
-if (envNode)
-{
-replace_string(&(information->argv[argIndex]),
-_strdup(_strchr(envNode->str, '=') + 1));
-continue;
-}
-
-replace_string(&(information->argv[argIndex]), _strdup(""));
-}
-
-return (0);
+  return (0);
 }
 
 /**
@@ -178,7 +170,7 @@ return (0);
 */
 int replace_string(char **old_str, char *new_str)
 {
-free(*old_str);
-*old_str = new_str;
-return (1);
+  free(*old_str);
+  *old_str = new_str;
+  return (1);
 }
